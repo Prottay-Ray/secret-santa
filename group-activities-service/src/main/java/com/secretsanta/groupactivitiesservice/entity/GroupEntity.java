81321cc3ff7f560eacd0cd684332df3b7d00d7c1
Prottay Ray <prottayray@gmail.com>
2021-12-15 13:54:37 +0530

    Group entity done

--- group-activities-service/src/main/java/com/secretsanta/groupactivitiesservice/entity/GroupEntity.java
+++ group-activities-service/src/main/java/com/secretsanta/groupactivitiesservice/entity/GroupEntity.java
@@ -1,4 +1,28 @@
 package com.secretsanta.groupactivitiesservice.entity;
 
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class GroupEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long groupId;
+    private String groupName;
+    private String groupTitle;
+    private Date budgetDeadline;
+    private Date wishlistDeadline;
+    private Double budgetAmount;
+
 }
