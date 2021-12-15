a28a28e0e56c20419174778c65b42e8bd0d84a02
Prottay Ray <prottayray@gmail.com>
2021-12-15 14:34:25 +0530

    communicator service created

---
+++ group-activities-service/src/main/java/com/secretsanta/groupactivitiesservice/service/CommunicatorService.java
@@ -0,0 +1,27 @@
+package com.secretsanta.groupactivitiesservice.service;
+
+public class CommunicatorService {
+
+    //This class is responsible for making all communications between
+    //multiple microservices and transacting the required details
+    //to and from the other parts of the service layer
+
+    public String getUniqueGroupName() {
+
+        return "";
+
+    }
+
+    public Boolean isCustomGroupNameAvailable(String customName) {
+
+        return false;
+
+    }
+
+    public Boolean assignGroupName(String groupName) {
+
+        return true;
+
+    }
+
+}
