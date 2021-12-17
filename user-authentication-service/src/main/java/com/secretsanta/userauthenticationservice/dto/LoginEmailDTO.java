86d47d5aa6fb88cc326428fa65431d919d0eb01b
Gautam Ankit Singh <ga3531767@gmail.com>
2021-12-17 20:19:46 +0530

    login authentication

---
+++ user-authentication-service/src/main/java/com/secretsanta/userauthenticationservice/dto/LoginEmailDTO.java
@@ -0,0 +1,15 @@
+package com.secretsanta.userauthenticationservice.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+
+public class SignupDTO {
+    private String Password;
+    private String Email;
+}
